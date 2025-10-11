a86ddb51368474dc71702a07b39628e54c1205a6
Julian Scharf <julzelements@gmail.com>
2025-10-11 15:37:31 +1000

    fixed test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    exclude: ["tests/**/*", "node_modules/**/*"],
   },
   resolve: {
     alias: {
